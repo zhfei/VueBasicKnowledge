7e1f4194d5d8dcd4d87e2dd36d0cf0ecfa61b616
zhfei <1174977961@qq.com>
2022-12-11 20:58:50 +0800

    add: 添加Vue组件生命周期监听

--- vue-component-demo/src/App.vue
+++ vue-component-demo/src/App.vue
@@ -5,18 +5,20 @@
     <Left/>
     <MyLeft></MyLeft>
     <MyRight></MyRight>
+    <Test></Test>
   </div>
 </template>
 
 <script>
 // 1.导入组件
 import Left from '@/components/Left.vue'
-
+import Test from '@/components/Test.vue'
 
 export default {
   components: {
     // 2.注册组件
-    Left
+    Left,
+    Test
   }
 };
 </script>
---
+++ vue-component-demo/src/components/Test.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p>hello vue.js</p>
+    </div>
+</template>
+
+<script>
+import { url } from 'inspector';
+
+export default {
+
+    data() {
+        return {
+            list:[]
+        };
+    },
+
+    methods: {
+        getBookList() {
+            var xml = new XMLHttpRequest()
+            xml.addEventListener('load',function(res) {
+                console.log(res);
+            })
+            xml.open('GET', 'http://www.liulongbin.top:3006/api/getbooks')
+            xml.send()
+        }
+    },
+
+    created () {
+        this.getBookList()
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
