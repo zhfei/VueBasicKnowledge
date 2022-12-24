e3f383c96e94e60a71055b42f90b8dcbeed82669
zhfei <1174977961@qq.com>
2022-12-24 12:58:39 +0800

    add: 添加文章组件

---
+++ vue-advanced-component-deom/src/components/Article/Article.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="article-container">
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueAdvancedComponentDeomArticle',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
