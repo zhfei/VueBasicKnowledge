46019f39b6a73b0211701f4c520b82aad12a9c32
zhfei <1174977961@qq.com>
2022-12-26 22:56:46 +0800

    other： 将axios添加到Vue的prototype实例对象上

--- vue-eslint-demo/src/main.js
+++ vue-eslint-demo/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 设置axios的域名根路径
+axios.defaults.baseURL = 'http//:www.top50.com'
+// 将axios对象挂载到Vue的原型对象上，这样每个Vue实例都可以通过this.axios访问的到。
+Vue.prototype.axios = axios
 
 new Vue({
   render: h => h(App)
