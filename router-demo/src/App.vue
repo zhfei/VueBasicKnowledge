3bdf1a7f83a2d1e07fbf1434e152dafcaba8cdb6
zhfei <1174977961@qq.com>
2022-12-31 17:43:41 +0800

    feat: 使用router-link代替a标签

--- router-demo/src/App.vue
+++ router-demo/src/App.vue
@@ -2,9 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
 
-    <a href="#/home">主页</a>
+    <!-- <a href="#/home">主页</a>
     <a href="#/center">个人中心</a>
-    <a href="#/about">关于</a>
+    <a href="#/about">关于</a> -->
+
+    <!-- 在vue-router中，使用router-link代替a标签，并且里面的to属性表示要跳转的路由地址 -->
+    <router-link to="/home">主页</router-link>
+    <router-link to="/center">个人中心</router-link>
+    <router-link to="/about">关于</router-link>
 
     <!-- <component :is="comName"></component> -->
     <!-- 使用vue-router默认就存在这个占位组件，这个是路由视图的出口 -->
--- router-demo/src/router/index.js
+++ router-demo/src/router/index.js
@@ -9,6 +9,7 @@ Vue.use(Router)
 
 // 创建路由的实例对象
 const router = new Router({
+  // 路由匹配规则，实例对应是根据匹配规则，从上到下逐条匹配的
   routes: [
     { path: '/home', component: Home },
     { path: '/about', component: About },
