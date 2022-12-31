58334f39af57bf9957a8305ee8f74a9127ea4f40
zhfei <1174977961@qq.com>
2022-12-31 17:49:13 +0800

    feat: 添加地址重定向匹配规则

--- router-demo/src/router/index.js
+++ router-demo/src/router/index.js
@@ -9,8 +9,11 @@ Vue.use(Router)
 
 // 创建路由的实例对象
 const router = new Router({
-  // 路由匹配规则，实例对应是根据匹配规则，从上到下逐条匹配的
+
   routes: [
+    // 地址重定向，当跳转的URL地址没有对应组件时，那么就会强制跳转到另一个地址
+    { path: '/', redirect: '/home' },
+    // 路由匹配规则，实例对应是根据匹配规则，从上到下逐条匹配的
     { path: '/home', component: Home },
     { path: '/about', component: About },
     { path: '/center', component: Center }
