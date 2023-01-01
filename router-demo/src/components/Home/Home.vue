3fed93e586c7fe9915f22dbe0db5edcb61de61ff
zhfei <1174977961@qq.com>
2023-01-01 23:34:28 +0800

    feat: 添加编程式导航

--- router-demo/src/components/Home/Home.vue
+++ router-demo/src/components/Home/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home-container">
-    Home组件
+    Home组件-编程式导航
+
+    <hr>
+    <button @click="handlePush">push:调用</button>
+    <button @click="handleReplace">replace:调用</button>
+    <button @click="handleGo">go:调用</button>
+    <!-- 行内使用编程式导航，要省略this,否则报错 -->
+    <button @click="$router.forward()">go便捷调用-forward前进一步</button>
+    <button @click="$router.back()">go便捷调用-back后退一步</button>
+
   </div>
 </template>
 
@@ -19,6 +28,18 @@ export default {
   },
 
   methods: {
+    handlePush () {
+      // this.$router.push('hash地址')， 会产生历史记录
+      this.$router.push('/about/tab2')
+    },
+    handleReplace () {
+      // this.$router.replace('hash地址')，跳转到目标地址，并替代掉当前的历史记录
+      this.$router.replace('/center/movie/3')
+    },
+    handleGo () {
+      // this.$router.go(-1) go(正数)向前， go(负数)后退
+      this.$router.go(-1)
+    }
 
   }
 }
