155c9b98f62540ba0a63096dcc39460d0bfe55e9
zhfei <1174977961@qq.com>
2023-01-13 23:41:53 +0800

    other: 把Vant组件库注册到Vue的插件

--- toutiao-demo/src/App.vue
+++ toutiao-demo/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     App根组件
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
--- toutiao-demo/src/main.js
+++ toutiao-demo/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 1. 引入你需要的组件
+import Vant from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+
+// 将Vant组件库注册到Vue插件中
+Vue.use(Vant)
 
 new Vue({
   router,
